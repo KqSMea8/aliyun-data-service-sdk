254ee9995be06bfd68af924d4a0e434e8955b605
子沭 <zishu.lf@alibaba-inc.com>
2019-03-13 13:07:24 +0800

    init sdk

---
+++ src/main/java/com/aliyun/dataworks/dataservice/common/constants/CommonConstants.java
@@ -0,0 +1,17 @@
+package com.aliyun.dataworks.dataservice.common.constants;
+
+/**
+ * @author lizheng
+ * @date 2017/9/29
+ */
+public interface CommonConstants {
+
+	String BASEID = "baseId";
+	String APPCODE = "appCode";
+	String APPKEY = "appKey";
+	String APPSECRET = "appSecret";
+    String X_CA_SIGNATURE = "X-Ca-Signature";
+    String X_CA_KEY = "X-Ca-Key";
+    String X_CA_TIMESTAMP = "X-Ca-Timestamp";
+	String DEFAULT_CHARSET = "utf-8";
+}
